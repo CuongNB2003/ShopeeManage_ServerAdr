15a2a856aa96df2316dfb9670251080aaf351151
cuongnbph22662 <bacuong15423@gmail.com>
2023-07-13 23:52:41 +0700

    Update user.api.js

--- controllers/API/user.api.js
+++ controllers/API/user.api.js
@@ -66,33 +66,30 @@ exports.list = async (req, res, next) => {
 exports.add = async (req, res, next) => {
     try {
         let role = await myDB.roleModel.findOne({ name: 'user' })
-        if (req.method == "POST") {
-            let obj = new myDB.userModel()
-            obj.username = req.body.username
-            obj.password = req.body.password
-            obj.fullname = req.body.fullname
-            obj.age = req.body.age
-            obj.email = req.body.email
-            obj.phone = req.body.phone
-            obj.address = req.body.address
-            obj.acc_status = true
-            obj.id_role = role._id
-            try {
-                if (req.file) {
-                    fs.renameSync(req.file.path, './public/avata_upload/' + req.file.originalname)
-                    obj.avata = '/avata_upload/' + req.file.originalname
-                }
-            } catch (error) {
-                console.log("================= Ảnh lỗi rồi m ơi: " + error.message);
+        let obj = new myDB.userModel()
+        obj.username = req.body.username
+        obj.password = req.body.password
+        obj.fullname = req.body.fullname
+        obj.age = req.body.age
+        obj.email = req.body.email
+        obj.phone = req.body.phone
+        obj.address = req.body.address
+        obj.acc_status = true
+        obj.id_role = role._id
+        try {
+            if (req.file) {
+                fs.renameSync(req.file.path, './public/avata_upload/' + req.file.originalname)
+                obj.avata = '/avata_upload/' + req.file.originalname
             }
-
-            let new_user = await obj.save()
-            return res.status(201).json({
-                msg: " Successful Add User",
-                data: new_user
-            })
+        } catch (error) {
+            console.log("================= Ảnh lỗi rồi m ơi: " + error.message);
         }
 
+        let new_user = await obj.save()
+        return res.status(201).json({
+            msg: " Successful Add User",
+            data: new_user
+        })
     } catch (error) {
         console.log(error);
         return res.status(500).json({
@@ -155,15 +152,15 @@ exports.login = async (req, res, next) => {
     try {
         let checkLogin = false;
         let inforUser = await myDB.userModel.findOne({ username: req.query.username });
-        if(inforUser){
-            if(inforUser.password == req.query.password){
+        if (inforUser) {
+            if (inforUser.password == req.query.password) {
                 checkLogin = true
                 return res.status(200).json({
                     inforUser: inforUser,
                     msg: "Đăng nhập thành công",
                     checkLogin: checkLogin
                 })
-            }else{
+            } else {
                 return res.status(203).json({
                     msg: "Sai mật khẩu",
                     checkLogin: checkLogin
